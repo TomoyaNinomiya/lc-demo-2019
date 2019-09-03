52f3a198d3f6cdb10d2b9b3a03b339a5f62574e7
Tomoya Ninomiya <ninomiya@sterfield.co.jp>
2019-09-03 15:18:54 +0900

    finish top page

--- dev_src/js/modules/component.js
+++ dev_src/js/modules/component.js
@@ -9,6 +9,14 @@ export default () => {
     $('.js-closeModal').on('click', () => {
       $('.l-modal').removeClass('is-active')
     })
+
+    if ($('.mainvisual').length) {
+      $('.mainvisual').slick({
+        infinite: false,
+        arrows: false,
+        dots: true
+      });
+    }
     
   })
 
